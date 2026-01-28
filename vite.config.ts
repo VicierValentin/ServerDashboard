43e843a9ba2c4d447c53969df0f4b43f43ad90ef
vvicier <vvicier@madic.com>
2026-01-28 14:02:19 +0100

    fix: add allowedHosts configuration to server settings in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: '0.0.0.0',
+      allowedHosts: ['vvicier-nextcloud.minmi-gar.ts.net'],
       proxy: {
         '/api': {
           target: `http://${apiHost}:3001`,
